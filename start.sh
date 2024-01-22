7e286018773a8330f7c82cede330723d99b6ef5f
0xhakis <zl.skylake@gmail.com>
2024-01-22 03:09:40 +0000

    [fix] frp

--- start.sh
+++ start.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-# 创建 FRP 配置文件
-cat << EOF > /root/frpc-config.ini
-[common]
-server_addr = ${FRPC_SERVER}
-server_port = ${FRPC_SERVER_PORT}
-token = ${FRPC_TOKEN}
+# 定义端口变量
+LOCAL_PORT=8848
 
-[test]
-type = ${FRPC_TYPE}
-local_ip = ${FRPC_LOCAL}
-local_port = ${FRPC_LOCAL_PORT}
+# 生成随机的8位字符串
+random_string=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8 ; echo '')
+tcp_name="tcp_$random_string"
+udp_name="udp_$random_string"
+
+# 创建 FRP 配置文件 (TOML 格式)
+cat << EOF > /root/frpc-config.toml
+[common]
+server_addr = "${FRPC_SERVER}"
+server_port = ${FRPC_SERVER_PORT}
+token = "${FRPC_TOKEN}"
+
+[${tcp_name}]
+type = "tcp"
+local_ip = "127.0.0.1"
+local_port = ${LOCAL_PORT}
+remote_port = ${FRPC_REMOTE_PORT}
+
+[${udp_name}]
+type = "udp"
+local_ip = "127.0.0.1"
+local_port = ${LOCAL_PORT}
 remote_port = ${FRPC_REMOTE_PORT}
 EOF
 
 # 启动 Shadowsocks server
-/usr/local/bin/ssserver -s "${SSSERVER_ADDR}" -m "${SSSERVER_METHOD}" -k "${SSSERVER_PASSWD}" &
+/usr/local/bin/ssserver -s "[::]:${LOCAL_PORT}" -m "${SSSERVER_METHOD}" -k "${SSSERVER_PASSWD}" &
 
 # 启动 FRP
-/usr/local/bin/frpc -c /root/frpc-config.ini &
+/usr/local/bin/frpc -c /root/frpc-config.toml &
 
 # 防止容器退出
 wait
+
 
